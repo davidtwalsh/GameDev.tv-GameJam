11af4137ac9b19013fc1b5fd63fe77f927a90632
davidtwalsh <davidtwalsh22@gmail.com>
2024-05-31 21:58:16 -0400

    Can send a wave of monsters

--- Assets/Scripts/Environment/SpawnController.cs
+++ Assets/Scripts/Environment/SpawnController.cs
@@ -13,6 +13,15 @@ public class SpawnController : MonoBehaviour
 
     private List<GameObject> monsters = new List<GameObject>();
 
+    private bool hasStarted = false;
+
+    private float gameTimer = 0f;
+
+    private float checkWavesTimer = 0f;
+
+    [SerializeField]
+    private List<Wave> waves = new List<Wave>();
+
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -38,6 +47,23 @@ public class SpawnController : MonoBehaviour
         {
             SpawnGoblin();
         }
+
+        if (hasStarted == true)
+        {
+            gameTimer += Time.deltaTime;
+            checkWavesTimer += Time.deltaTime;
+
+            if (checkWavesTimer > .3f)
+            {
+                Wave wave = GetWaveForTime((int)Mathf.Floor(gameTimer));
+                if (wave != null)
+                {
+                    SpawnForWave(wave);
+                    waves.Remove(wave);
+                }
+                checkWavesTimer = 0f;
+            }
+        }
     }
 
     private void SpawnGoblin()
@@ -50,8 +76,32 @@ public class SpawnController : MonoBehaviour
         int randomIndex = Random.Range(0, spawns.Count);
         GameObject monster = Instantiate(goblinPrefab, spawns[randomIndex], Quaternion.identity);
         monsters.Add(monster);
-        
     }
+    
+    private void SpawnForWave(Wave wave)
+    {
+        List<Vector3> spawnsCopy = new List<Vector3>();
+        foreach (Vector3 spawner in spawns)
+        {
+            spawnsCopy.Add(spawner);
+        }
+
+        foreach (MonsterTypeInWave monsterType in wave.monsters)
+        {
+            for (int i = 0; i < monsterType.monsterCount; i++)
+            {
+                if (spawnsCopy.Count == 0)
+                {
+                    Debug.LogError("Wave is too big! no more spawns");
+                }
+                int randomIndex = Random.Range(0, spawnsCopy.Count);
+                GameObject monster = Instantiate(monsterType.monsterPrefab, spawns[randomIndex], Quaternion.identity);
+                monsters.Add(monster);
+                spawnsCopy.RemoveAt(randomIndex);
+            }
+        }
+    }
+
     private void GetSpawns()
     {
         spawns = new List<Vector3>();
@@ -73,4 +123,36 @@ public class SpawnController : MonoBehaviour
     {
         return monsters;
     }
+
+    public void StartGame()
+    {
+        hasStarted = true;
+    }
+
+    public Wave GetWaveForTime(int time)
+    {
+        foreach (Wave wave in waves)
+        {
+            if (wave.waveTime == time)
+            {
+                return wave;
+            }
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class Wave
+{
+    public int waveTime;
+    public List<MonsterTypeInWave> monsters;
+    
+}
+
+[System.Serializable]
+public class MonsterTypeInWave
+{
+    public GameObject monsterPrefab;
+    public int monsterCount;
 }
